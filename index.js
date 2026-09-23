3f0b1415bf450d420c66a91126a672ce448afe47
Matthias Groß <mg-rs@gmx.de>
2026-09-23 15:16:44 +0200

    fix first pin

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ console.log('Happy developing ✨')
 // Where you want to render the map.
 var element = document.getElementById('osm-map');
 
+var raccoonIcon = L.icon({
+    iconUrl: 'raccoon.png',
+
+    iconSize:     [100, 100], // size of the icon
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
 const map = L.map(element).setView([51.16, 10.45], 6); // roughly centered on Germany
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
-L.marker([51.1707612, 7.1820499]).addTo(map).bindPopup('mapfe')
+L.marker([51.1707612, 7.1820499], {icon: raccoonIcon}).addTo(map).bindPopup('mapfe')

976d9d4ff9468a0898de525ac9f8bfa9fa15eefd
Matthias Groß <mg-rs@gmx.de>
2026-09-23 15:10:44 +0200

    add first pin

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 console.log('Happy developing ✨')
+// Where you want to render the map.
+var element = document.getElementById('osm-map');
+
+const map = L.map(element).setView([51.16, 10.45], 6); // roughly centered on Germany
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+L.marker([51.1707612, 7.1820499]).addTo(map).bindPopup('mapfe')

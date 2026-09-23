e3db6cc72b98fcc6266f7915c84913efacf42d50
Matthias Groß <mg-rs@gmx.de>
2026-09-23 15:26:11 +0200

    style test pin

--- index.js
+++ index.js
@@ -12,4 +12,5 @@ var raccoonIcon = L.icon({
 
 const map = L.map(element).setView([51.16, 10.45], 6); // roughly centered on Germany
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
-L.marker([51.1707612, 7.1820499], {icon: raccoonIcon}).addTo(map).bindPopup('mapfe')
+const addressString = "Thomasstraße 1<br>42857 Remscheid"
+L.marker([51.1707612, 7.1820499], {icon: raccoonIcon}).addTo(map).bindPopup('Mapfe<br>' + addressString);
